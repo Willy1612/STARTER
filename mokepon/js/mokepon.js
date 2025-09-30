ca191ddbcc28bdb74af9b76cef9287f0b35999f9
Willy1612 <guillemseguramoreno@gmail.com>
2025-10-01 00:04:23 +0200

    mokepon1.11

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,35 @@
-function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE TU MASCOTA")
-
+function iniciarJuego (){
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click" , seleccionarMascotaJugador)
 }
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click" , seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador() {
+    let inputXavi=document.getElementById("Xavi")
+    let inputVinicius=document.getElementById("Vinicius")
+    let inputPedri=document.getElementById("Pedri")
+
+    let mascotaElegida = ""
+        if(inputXavi.checked)
+            {mascotaElegida="Xavi"
+            alert("Has elegido: " + mascotaElegida)
+
+            }
+        else if(inputVinicius.checked) 
+            {mascotaElegida="Vinicius"
+            alert("Has elegido: " + mascotaElegida)
+
+            }
+        else if(inputPedri.checked)
+            {mascotaElegida="Pedri"
+            alert("Has elegido: " + mascotaElegida)
+
+            }
+        else{
+            alert("Primero elige una mascota")
+        }
+
+    
+}
+
+
+window.addEventListener("load", iniciarJuego)
