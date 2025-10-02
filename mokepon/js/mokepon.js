cdb9a82b445968297a5514e66f0b63fcdf3e05ad
Willy1612 <guillemseguramoreno@gmail.com>
2025-10-02 22:48:29 +0200

    mokepon1.12

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -8,6 +8,7 @@ function seleccionarMascotaJugador() {
     let inputVinicius=document.getElementById("Vinicius")
     let inputPedri=document.getElementById("Pedri")
 
+    
     let mascotaElegida = ""
         if(inputXavi.checked)
             {mascotaElegida="Xavi"
